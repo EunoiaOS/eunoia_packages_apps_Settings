807f033b6c4e92f74f67917170cd889899fd1bca
EunoiaOS <235215905+eunoia-committer@users.noreply.github.com>
2025-12-01 04:34:42 +0700

    EunoiaOS
    Change-Id: Ia91b1b236dd798b38652c940b8283b92bb03075e

--- src/com/android/settings/deviceinfo/firmwareversion/EunoiaVersionDetailPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/EunoiaVersionDetailPreferenceController.java
@@ -35,17 +35,13 @@ import com.android.settings.core.BasePreferenceController;
 import com.android.settingslib.RestrictedLockUtils;
 import com.android.settingslib.RestrictedLockUtilsInternal;
 
-public class LineageVersionDetailPreferenceController extends BasePreferenceController {
+public class EunoiaVersionDetailPreferenceController extends BasePreferenceController {
 
-    private static final String TAG = "lineageVersionDialogCtrl";
+    private static final String TAG = "eunoiaVersionDialogCtrl";
     private static final int DELAY_TIMER_MILLIS = 500;
     private static final int ACTIVITY_TRIGGER_COUNT = 3;
 
-    private static final String KEY_LINEAGE_VERSION_PROP = "ro.lineage.display.version";
-
-    private static final String PLATLOGO_PACKAGE_NAME = "org.lineageos.lineageparts";
-    private static final String PLATLOGO_ACTIVITY_CLASS =
-            PLATLOGO_PACKAGE_NAME + ".logo.PlatLogoActivity";
+    private static final String KEY_EUNOIA_VERSION_PROP = "ro.eunoia.display.version";
 
     private final UserManager mUserManager;
     private final long[] mHits = new long[ACTIVITY_TRIGGER_COUNT];
@@ -53,7 +49,7 @@ public class LineageVersionDetailPreferenceController extends BasePreferenceCont
     private RestrictedLockUtils.EnforcedAdmin mFunDisallowedAdmin;
     private boolean mFunDisallowedBySystem;
 
-    public LineageVersionDetailPreferenceController(Context context, String key) {
+    public EunoiaVersionDetailPreferenceController(Context context, String key) {
         super(context, key);
         mUserManager = (UserManager) mContext.getSystemService(Context.USER_SERVICE);
         initializeAdminPermissions();
@@ -76,39 +72,8 @@ public class LineageVersionDetailPreferenceController extends BasePreferenceCont
 
     @Override
     public CharSequence getSummary() {
-        return SystemProperties.get(KEY_LINEAGE_VERSION_PROP,
+        return SystemProperties.get(KEY_EUNOIA_VERSION_PROP,
                 mContext.getString(R.string.unknown));
-    }
-
-    @Override
-    public boolean handlePreferenceTreeClick(Preference preference) {
-        if (!TextUtils.equals(preference.getKey(), getPreferenceKey())) {
-            return false;
-        }
-        if (Utils.isMonkeyRunning()) {
-            return false;
-        }
-        arrayCopy();
-        mHits[mHits.length - 1] = SystemClock.uptimeMillis();
-        if (mHits[0] >= (SystemClock.uptimeMillis() - DELAY_TIMER_MILLIS)) {
-            if (mUserManager.hasUserRestriction(UserManager.DISALLOW_FUN)) {
-                if (mFunDisallowedAdmin != null && !mFunDisallowedBySystem) {
-                    RestrictedLockUtils.sendShowAdminSupportDetailsIntent(mContext,
-                            mFunDisallowedAdmin);
-                }
-                Log.d(TAG, "Sorry, no fun for you!");
-                return true;
-            }
-
-            final Intent intent = new Intent(Intent.ACTION_MAIN)
-                     .setClassName(PLATLOGO_PACKAGE_NAME, PLATLOGO_ACTIVITY_CLASS);
-            try {
-                mContext.startActivity(intent);
-            } catch (Exception e) {
-                Log.e(TAG, "Unable to start activity " + intent.toString());
-            }
-        }
-        return true;
     }
 
     /**
